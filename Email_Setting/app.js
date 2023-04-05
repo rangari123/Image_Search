b2601299c0fee79f347ca57f926e22d776923e0d
rangari123 <avishkarrangari213@gmail.com>
2023-04-06 00:54:15 +0530

    first Commit

---
+++ Email_Setting/app.js
@@ -0,0 +1,6 @@
+const save = document.querySelector(".save");
+const text = document.querySelector(".sa");
+save.addEventListener("click",function(){
+ text.textContent="Your Information is  saved";
+ alert("You may Leave this window")
+})
